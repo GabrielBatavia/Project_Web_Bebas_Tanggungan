3f26215e595a06e6eb8116bacbf614f7b4d555ce
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-10-15 07:40:47 +0700

    feat: implement basic routing mechanism with controller and method handling

--- core/App.php
+++ core/App.php
@@ -1,2 +1,39 @@
 <?php
-// Core App class
+class App {
+    protected $controller = 'MahasiswaController';
+    protected $method = 'index';
+    protected $params = [];
+
+    public function __construct() {
+        $url = $this->parseUrl();
+        
+        // Check for controller existence
+        if (file_exists('../app/Controllers/' . $url[0] . 'Controller.php')) {
+            $this->controller = $url[0] . 'Controller';
+            unset($url[0]);
+        }
+        
+        require_once '../app/Controllers/' . $this->controller . '.php';
+        $this->controller = new $this->controller;
+
+        // Check for method existence
+        if (isset($url[1])) {
+            if (method_exists($this->controller, $url[1])) {
+                $this->method = $url[1];
+                unset($url[1]);
+            }
+        }
+
+        // Parameters
+        $this->params = $url ? array_values($url) : [];
+
+        // Call the method with parameters
+        call_user_func_array([$this->controller, $this->method], $this->params);
+    }
+
+    public function parseUrl() {
+        if (isset($_GET['url'])) {
+            return explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
+        }
+    }
+}
