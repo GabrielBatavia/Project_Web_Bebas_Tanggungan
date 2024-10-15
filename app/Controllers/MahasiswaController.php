32e90b0409e35f1731f597fa4016cc17c2fad31f
GabrielBatavia <gabrielbatavia7@gmail.com>
2024-10-15 10:38:51 +0700

    feat: add Controller base class and MahasiswaController with methods for viewing and updating student data

--- app/Controllers/MahasiswaController.php
+++ app/Controllers/MahasiswaController.php
@@ -1,2 +1,23 @@
 <?php
-// MahasiswaController content
+class MahasiswaController extends Controller {
+    private $mahasiswaModel;
+
+    public function __construct() {
+        $this->mahasiswaModel = $this->model('Mahasiswa');
+    }
+
+    public function index() {
+        $mahasiswa = $this->mahasiswaModel->getMahasiswaById(1);  // ID mahasiswa dummy
+        $this->view('dataMahasiswa', $mahasiswa);
+    }
+
+    public function update() {
+        $data = [
+            'student_id' => $_POST['student_id'],
+            'name' => $_POST['name'],
+            'major' => $_POST['major']
+        ];
+        $this->mahasiswaModel->updateMahasiswa($data);
+        header("Location: /mahasiswa");
+    }
+}
--- core/Controller.php
+++ core/Controller.php
@@ -1,2 +1,18 @@
 <?php
-// Core Controller class
+class Controller {
+    // Fungsi untuk memanggil model
+    public function model($model) {
+        require_once '../app/Models/' . $model . '.php';
+        $db = new Database(); // Buat instance dari Database
+        return new $model($db->connect()); // Panggil metode connect() dari instance
+    }
+    
+    // Fungsi untuk menampilkan view
+    public function view($view, $data = []) {
+        if (file_exists('../app/Views/' . $view . '.php')) {
+            include '../app/Views/' . $view . '.php';
+        } else {
+            die("View {$view} tidak ditemukan");
+        }
+    }
+}
